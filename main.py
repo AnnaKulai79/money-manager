bdb9eedbf23f0bba5ddc71532b084ac6dcfeb114
AnnaKulai79 <hannahku68@gmail.com>
2025-11-10 15:58:47 +0100

    write function

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import csv
-
 def add_item():
     name_produkt = input("Введіть назву товару: ").capitalize()
     count_produkt = int(input("Введіть кількість: "))
@@ -8,10 +6,12 @@ def add_item():
     return {"name": name_produkt, "count": count_produkt, "price": price_produkt}
 
 def show_list(sh_lst):
-    print("Ваш список покупок: ")
+    if not sh_lst:
+        print("\nВаш список покупок порожній")
+        return    
+    print("\nВаш список покупок: ")
     for i, el in enumerate(sh_lst):
         print(f"{i + 1}. {el["name"]}- {el["count"]}шт за ціною {el["price"]}")
-    return
 
 def sum_total(sh_lst):
     sum = 0
@@ -20,12 +20,9 @@ def sum_total(sh_lst):
     return print(f"Поточна сума ваших покупок становить: {sum}")
 
 def save_file(sh_lst, file_path):
-    with open(file_path, mode="w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=";")
+    with open(file_path, "a") as f: # a ==> for appending
         for elem in sh_lst:
-            row = [elem["name"], elem["count"], elem["price"]]
-            writer.writerow(row)
-    return
+            f.write(f"{elem["name"]},{elem["count"]},{elem["price"]}\n")
 
 def load_file(sh_lst, file_path):
     if sh_lst != []:
@@ -34,22 +31,22 @@ def load_file(sh_lst, file_path):
             ans = input("Ваш поточний список буде знищено(y/n)?")
             if ans == "":
                 sh_lst = []
-    with open(file_path) as csv_file:
-        reader = csv.reader(csv_file, delimiter=";")
-    for elem in reader:
-        item = {
-            "name": elem[0],
-            "count": elem[1],
-            "price": elem[3]
-            }
-        sh_lst.append(item)
-    return sh_lst
+    sh_l = []
+    with open(file_path, "r") as f: # r ==> open for reading
+        for line in f:
+            name, count, price = line.split(",")
+            sh_l.append({
+                "name": name,
+                "count": count,
+                "price": price,
+                })
+    return sh_l + sh_lst
 
 
 def main():
     print("🛒 Вітаю у менеджері покупок! ")
     shopping_list = []
-    file_path = './data/Shopping_list.csv'
+    file_path = './data/Shopping_list.txt'
     while True:
         print('''
     Меню:
@@ -60,43 +57,41 @@ def main():
     5. Завантажити з файлу
     6. Вихід
             ''')
+        try:
+            punkt_menu = int(input("Ваш вибір(1-6): "))
+            match punkt_menu:
+                case 1:
+                    shopping_list.append(add_item())
+                    answer = input("Перейти до меню (Y/N)?: ").lower()
+                    if answer == "n":
+                        break
+                case 2:
+                    show_list(shopping_list)
+                    answer = input("Перейти до меню (Y/N)?: ").lower()
+                    if answer == "n":
+                        break
+                case 3:
+                    sum_total(shopping_list)
+                    answer = input("Перейти до меню (Y/N)?: ").lower()
+                    if answer == "n":
+                        break
+                case 4:
+                    save_file(shopping_list,file_path)
+                    answer = input("Перейти до меню (Y/N)?: ").lower()
+                    if answer == "n":
+                        break
+                case 5:
+                    shopping_list = load_file(shopping_list, file_path)
+                    answer = input("Перейти до меню (Y/N): ").lower()
+                    if answer == "n":
+                        break
+                case 6:
+                    break   
+                case _:
+                    print("\nВведено не вірні данні! Введіть 1-6")
+                    continue 
+        except ValueError:
+            print("\nВведено не вірні данні! Введіть 1-6")
 
-        punkt_menu = int(input("Ваш вибір(1-6): "))
-        match punkt_menu:
-            case 1:
-                shopping_list.append(add_item())
-                answer = input("Перейти до меню (Y/N)?: ").lower()
-                if answer == "n":
-                    break
-                continue
-            case 2:
-                show_list(shopping_list)
-                answer = input("Перейти до меню (Y/N)?: ").lower()
-                if answer == "n":
-                    break
-                continue
-            case 3:
-                sum_total(shopping_list)
-                answer = input("Перейти до меню (Y/N)?: ").lower()
-                if answer == "n":
-                    break
-                continue
-            case 4:
-                save_file(shopping_list,file_path)
-                answer = input("Перейти до меню (Y/N)?: ").lower()
-                if answer == "n":
-                    break
-                continue
-            case 5:
-                shopping_list = load_file(shopping_list, file_path)
-                answer = input("Перейти до меню (Y/N): ").lower()
-                if answer == "n":
-                    break
-                continue
-            case 6:
-                break   
-            case _:
-                print("Введено не вірні данні! Введіть 1-6")
-                continue 
 
 main()
